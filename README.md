4c7aee585047da44cc927db6e8f52ab80cd11bee
Osama Jandali <osamajandaly@gmail.com>
2022-07-11 15:09:48 +0300

    init GSF docusaurus template

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to use this template
+
+Just edit `docusaurus.config.js` files where the comments are and you're ready to go.
---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  // Change to Site title
+  title: "GSF Site",
+  // Change to site description
+  tagline: "GSF are cool",
+  // Change to site url
+  url: "https://greensoftware.foundation/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Green-Software-Foundation",
+  // Change to GitHub repo name.
+  projectName: "GSF-project",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        // Change to project name
+        title: "Project",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            href: "https://greensoftware.foundation/",
+            position: "right",
+            label: "GSF website",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Links",
+            items: [
+              {
+                label: "Github",
+                href: "https://github.com/Green-Software-Foundation",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/gsfcommunity",
+              },
+              {
+                label: "Linkedin",
+                href: "https://www.linkedin.com/company/green-software-foundation/",
+              },
+            ],
+          },
+          {
+            title: "LEGAL",
+            items: [
+              {
+                label: "Trademark Policy",
+                href: "https://greensoftware.foundation/policy/trademark",
+              },
+              {
+                label: "Terms and Privacy Policy",
+                href: "https://greensoftware.foundation/policy/terms",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Membership enquires",
+                href: "https://share.hsforms.com/1NNvkCLgfS4GIUJ0XPH93iw4tvhy",
+              },
+              {
+                label: "Join us",
+                href: "https://greensoftware.foundation/join-us/",
+              },
+              {
+                label: "Helpdesk",
+                href: "https://greensoftware.foundation/helpdesk/",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Joint Development Foundation Projects, LLC, Green Software Foundation Series`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+      },
+      colorMode: {
+        disableSwitch: true,
+        defaultMode: "light",
+      },
+    }),
+};
+
+module.exports = config;
